87c24e8c57409c5eb1c1e871f6b708994cb99f5c
john <johns@accubits.com>
2019-10-15 10:44:13 +0530

    hello saga

--- packages/sdk/saga/hello.js
+++ packages/sdk/saga/hello.js
@@ -1,3 +1,10 @@
-export function* helloSaga() {
+import { takeEvery } from "redux-saga/effects";
+import { INCREMENT } from "../constants/actions";
+
+function* sayHello() {
   console.log("Hello Sagas!");
+}
+
+export function* helloSaga() {
+  yield takeEvery(INCREMENT, sayHello);
 }
