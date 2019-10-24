b0daee62eedef374e3f1b522316cf5f80166fcb9
John <johnjayasingh.s@gmail.com>
2019-10-24 14:40:36 +0530

    font

--- packages/mobile/Counter.js
+++ packages/mobile/Counter.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 import { increment, decrement } from "sdk/actions";
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, StyleSheet } from 'react-native';
 
 class Counter extends Component {
     render() {
@@ -13,16 +13,20 @@ class Counter extends Component {
                 alignItems: 'center',
                 justifyContent: 'center',
             }}>
-                <Text>Counter</Text>
+                <Text style={styles.bigFont}>Counter</Text>
                 <View>
-                    <Button onPress={this.props.decrement} title="-"></Button>
-                    <Text style={{ color: '#999' }}>{this.props.count}</Text>
-                    <Button onPress={this.props.increment} title="+"></Button>
+                    <Button style={styles.bigFont} onPress={this.props.decrement} title="-"></Button>
+                    <Text style={styles.bigFont}>{this.props.count}</Text>
+                    <Button style={styles.bigFont} onPress={this.props.increment} title="+"></Button>
                 </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    bigFont: { color: '#999', textAlign: "center", fontSize: 40 }
+})
 
 function mapStateToProps(state) {
     return {
