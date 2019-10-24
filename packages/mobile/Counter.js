b1410b8657b2b42e12274393c75e8eb7d9d27a93
John <johnjayasingh.s@gmail.com>
2019-10-24 14:22:16 +0530

    deps

---
+++ packages/mobile/Counter.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import { increment, decrement } from "sdk/actions";
+import { Text, View, Button } from 'react-native';
+
+class Counter extends Component {
+    render() {
+        return (
+            <View style={{
+                flex: 1,
+                backgroundColor: '#fff',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}>
+                <Text>Counter</Text>
+                <View>
+                    <Button onPress={this.props.decrement}>-</Button>
+                    <Text style={{ color: '#999' }}>{this.props.count}</Text>
+                    <Button onPress={this.props.increment}>+</Button>
+                </View>
+            </View>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        count: state.counter
+    };
+}
+
+const mapDispatchToProps = {
+    increment,
+    decrement
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Counter);
